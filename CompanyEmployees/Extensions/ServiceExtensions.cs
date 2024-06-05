2587f1483ed091442a33b0807ae2659bb85d9b49
tuanhungdev1 <tuanhungdev274@gmail.com>
2024-06-05 21:31:14 +0700

    Add methods to configure CORS and IIS

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace CompanyEmployees.Extensions {
+    public static class ServiceExtensions {
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options => {
+
+                
+                //Có thể sử dụng các hàm khác như WithOrigins("https://example.com") tường tự với WithMethods("POST", "GET") để chi cho phép các method
+                // Cũng tương tự với WithHeaders("accept", ""content-type)
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+                );
+            });
+    }
+}
--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,6 +1,10 @@
+using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -11,11 +15,22 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
+} else {
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions {
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
