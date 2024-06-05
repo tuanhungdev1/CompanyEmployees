9f2bfd4e7788258c1b5b1e8b67c0aab5a43a602d
tuanhungdev1 <tuanhungdev274@gmail.com>
2024-06-06 04:14:57 +0700

    Added configuration settings to customize logging behavior using Nlog.

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace CompanyEmployees.Extensions {
+﻿using Contracts;
+using LoggerService;
+
+namespace CompanyEmployees.Extensions {
     public static class ServiceExtensions {
         public static void ConfigureCors(this IServiceCollection services) =>
             services.AddCors(options => {
@@ -10,5 +13,13 @@
                     builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
                 );
             });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options => {
+
+            });
+
+        public static void ConfigureLoggerService(this IServiceCollection services) => services.AddSingleton<ILoggerManager, LoggerManager>();
+
     }
 }
--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,10 +1,27 @@
-using CompanyEmployees.Extensions;
+﻿using CompanyEmployees.Extensions;
 using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+// Đường dẫn tới tệp cấu hình
+var configFilePath = Path.Combine(AppContext.BaseDirectory, "./nlog.config.txt");
+
+// Kiểm tra xem tệp cấu hình có tồn tại không
+if (!File.Exists(configFilePath)) {
+    throw new FileNotFoundException($"NLog configuration file not found: {configFilePath}");
+}
+
+// Tải cấu hình NLog bằng phương thức mới
+LogManager.Setup().LoadConfigurationFromFile(configFilePath);
+
+
 builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
