eb291caee7a1957deaa17c0eab191957d941b503
tuanhungdev1 <tuanhungdev274@gmail.com>
2024-06-10 04:05:16 +0700

    Add controller Employee

--- Contracts/IEmployeeRepository.cs
+++ Contracts/IEmployeeRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,8 @@ using System.Threading.Tasks;
 
 namespace Contracts {
     public interface IEmployeeRepository {
+        IEnumerable<Employee> GetEmployees(Guid companyId, bool trackChanges);
+
+        Employee GetEmployee(Guid companyId, Guid id, bool trackChanges);
     }
 }
--- Service.Contracts/IEmployeeService.cs
+++ Service.Contracts/IEmployeeService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shared.DataTransferObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,11 @@ using System.Threading.Tasks;
 
 namespace Service.Contracts {
     public interface IEmployeeService {
+
+        IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges);
+
+        EmployeeDto GetEmployee(Guid employeeId, Guid id, bool trackChanges);
     }
+
+    
 }
