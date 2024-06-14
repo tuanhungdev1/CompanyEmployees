9a3a8d9274ff100065b866403320338633015916
tuanhungdev1 <tuanhungdev274@gmail.com>
2024-06-14 21:25:07 +0700

    Add async task for controller, service, repository,...

--- Contracts/ICompanyRepository.cs
+++ Contracts/ICompanyRepository.cs
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 
 namespace Contracts {
     public interface ICompanyRepository {
-        IEnumerable<Company> GetAllCompanies(bool trackChanges);
+        Task<IEnumerable<Company>> GetAllCompaniesAsync(bool trackChanges);
 
-        Company GetCompany(Guid companyId, bool trackChanges);
+        Task<Company> GetCompanyAsync(Guid companyId, bool trackChanges);
 
         void CreateCompany(Company company);
 
-        IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges);
+        Task<IEnumerable<Company>> GetByIdsAsync(IEnumerable<Guid> ids, bool trackChanges);
 
         void DeleteCompany(Company company);
     }
--- Contracts/IRepositoryManager.cs
+++ Contracts/IRepositoryManager.cs
@@ -9,6 +9,6 @@ namespace Contracts {
         ICompanyRepository Company { get; }
         IEmployeeRepository Employee { get; }
 
-        void Save();
+        Task SaveAsync();
     }
 }
